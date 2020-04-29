9c1dbd5261dcc5e2b7dee4846006a92d2b24b7d1
Franky <hoangtuan1501@gmail.com>
2020-04-29 15:44:16 +0700

    Implement task status update for Mongo repository

--- todos/repos/mongo_repos/__init__.py
+++ todos/repos/mongo_repos/__init__.py
@@ -55,3 +55,60 @@ class MongoRepos:
                 item['emergency']
             ))
         return result
+
+    def find_by_id(self, task_id):
+        doc = self.mongo_col.find_one(
+            {
+                'tasks.id': task_id
+            }, {
+                'tasks': {
+                    '$elemMatch': {'id': task_id}
+                }
+            }
+        )
+        task_obj = doc['tasks'][0]
+        return Task(
+            task_obj['id'],
+            task_obj['desc'],
+            task_obj['state'],
+            task_obj['emergency']
+        )
+
+    def find_user_by_username(self, username):
+        doc = self.mongo_col.find_one({'username': username}, {'_id': 0})
+
+        user = User(username)
+        user.done_task = doc.get('done', 0)
+        user.doing_task = doc.get('doing', 0)
+        return user
+
+    def mark_task_as_doing(self, username, task_id):
+        self.mongo_col.update_one(
+            {
+                'username': username,
+                'tasks.id': task_id
+            }, {
+                '$set': {
+                    'tasks.$.state': 'DOING'
+                },
+                '$inc': {
+                    'doing': 1
+                }
+            }
+        )
+
+    def mark_task_as_done(self, username, task_id):
+        self.mongo_col.update_one(
+            {
+                'username': username,
+                'tasks.id': task_id
+            }, {
+                '$set': {
+                    'tasks.$.state': 'DONE'
+                },
+                '$inc': {
+                    'done': 1,
+                    'doing': -1
+                }
+            }
+        )
